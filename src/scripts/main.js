634ed2102d6b104dbc66d1acd81482f6b7baa48c
Austin Hudson <arhudson7@gmail.com>
2017-02-12 10:08:55 -0600

    added to where you use the line, square, circle and triangle button to draw presized shapes, only the current client sees what they are drawing these

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,12 +29,13 @@ document.addEventListener("DOMContentLoaded", function(){
   tools.square = squareTool;
   tools.circle = circleTool;
   tools.triangle = triangleTool;
-  tools.lineTool = lineTool;
+  tools.line = lineTool;
 
   //listeners
   canvas.addEventListener("mousedown", canvasEvent, false);
   canvas.addEventListener("mousemove", canvasEvent, false);
   canvas.addEventListener("mouseup", canvasEvent, false);
+
 
   pencilBtn.addEventListener("click", function(event){
     toolToggle(event);
@@ -72,21 +73,63 @@ document.addEventListener("DOMContentLoaded", function(){
   function line() {
     let tool = this;
     this.started = false;
+
+    this.mousedown = function(event){
+      tool.started = true;
+      ctx.mouseWidth = 12;
+      ctx.strokeStyle = "orange";
+      ctx.beginPath();
+      ctx.moveTo(event.x, event.y);
+    }
+
+    this.mouseup = function(event){
+      if(tool.started){
+        ctx.lineTo(event.x, event.y);
+        ctx.stroke();
+        tool.started = false;
+      }
+    }
   }
 
   function triangle() {
     let tool = this;
     this.started = false;
+
+    this.mousedown = function(event){
+      ctx.mouseWidth = 12;
+      ctx.strokeStyle = "purple";
+      ctx.beginPath();
+      ctx.moveTo(event.x, event.y);
+      ctx.lineTo(event.x - 50, event.y + 50);
+      ctx.lineTo(event.x + 50, event.y + 50);
+      ctx.closePath();
+      ctx.stroke();
+    }
   }
 
   function circle() {
     let tool = this;
     this.started = false;
+
+    this.mousedown = function(event){
+      ctx.mouseWidth = 12;
+      ctx.strokeStyle = "blue";
+      ctx.beginPath();
+      ctx.arc(event.x, event.y, 30, 0, 2*Math.PI);
+      ctx.stroke();
+    }
   }
 
   function square() {
     let tool = this;
     this.started = false;
+
+    this.mousedown = function(event){
+      ctx.mouseWidth = 12;
+      ctx.strokeStyle = "green";
+      ctx.beginPath();
+      ctx.strokeRect(event.x, event.y, 75, 75);
+    }
   }
 
   function pencil(){
@@ -148,6 +191,7 @@ document.addEventListener("DOMContentLoaded", function(){
     p.appendChild(userStatus);
     userPanel.appendChild(p);
   });
+
   //user has disconnected
   socket.on("user-disconnect", function(data){
     //Edit the DOM
@@ -158,6 +202,7 @@ document.addEventListener("DOMContentLoaded", function(){
     p.appendChild(userStatus);
     userPanel.appendChild(p);
   });
+
   //client has drawn something
   socket.on('draw-new', function(data){
     ctx.mouseWidth = 12;
@@ -165,6 +210,7 @@ document.addEventListener("DOMContentLoaded", function(){
     ctx.lineTo(data.line[0], data.line[1]);
     ctx.stroke();
   });
+
   //new client has join a session, update them with current state of canvas
   socket.on('draw-current', function(data){
     ctx.mouseWidth = 12;
@@ -175,7 +221,6 @@ document.addEventListener("DOMContentLoaded", function(){
         ctx.lineTo(data[coord][i][0], data[coord][i][1]);
         ctx.stroke();
       }
-
     }
 
   });
